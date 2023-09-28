e10e980547a5b25158ab584cea612f4312bac434
Roopk9129 <roopk9129@gmail.com>
2023-09-28 09:37:57 +0530

    consultation fee details added

--- src/test/java/com/HMS/Doctor/CreateDoctor.java
+++ src/test/java/com/HMS/Doctor/CreateDoctor.java
@@ -4,6 +4,7 @@ public class CreateDoctor {
 
 	public static void main(String[] args) {
 		System.out.println("Doctor has been created");
+		System.out.println("Doctor has been created with consultation fee details");
 	}
 
 }
